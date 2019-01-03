60430f72fc6d0406687bd9b8690881b4783daa6e
Alexander Kozarev <al1102@yandex.ru>
2019-01-03 16:49:48 +0500

    static

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIR = {
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'template_static'),
-}
+]
 
 APPEND_SLASH = False
