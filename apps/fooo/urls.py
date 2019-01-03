90ca593571ca01ba1f26be9c422a0ec03623d2be
Alexander Kozarev <al1102@yandex.ru>
2019-01-03 16:14:59 +0500

    some fixes

--- apps/fooo/urls.py
+++ apps/fooo/urls.py
@@ -5,8 +5,8 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='My API')
 
 urlpatterns = [
-    path("", schema_view),
-    # path("", views.interface, name="interface"),
+    # path("", schema_view),
+    path("", views.interface, name="interface"),
     path("interface/", views.interface, name="interface"),
     path("new_item/", views.new_item, name="new_item"),
     path("del_item/", views.del_item, name="del_item"),
--- config/settings.py
+++ config/settings.py
@@ -123,10 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_DIR = {
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'template_static'),
 }
 
 APPEND_SLASH = False
